65f499903eea2318870cf36f243c21e7217be4ab
Dennis Onder <35642244+DennisOnder@users.noreply.github.com>
2018-07-18 20:00:23 +0200

    Add files via upload
    Implemented Local Storage.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
+window.addEventListener('DOMContentLoaded', function() {
+    fetchTodos();
+    completeButton();
+    removeButton();
+});
+
 // Variables
 var input = document.getElementById('inputField');
 var inputButton = document.getElementById('inputBtn');
 var placeForTodos = document.getElementById('todos');
+var markup = '<div class="todo">' +
+    '<div class="well">' +
+    '<h3 class="todo-text" id="id">';
+var markupend = '</h3>' +
+    '<input type="button" class="btn btn-outline-light remBtn" value="Delete">' +
+    '<input type="button" class="btn btn-outline-light completeBtn" value="Complete">' +
+    '</div>' +
+    '</div>';
 
-// Call functions
-completeButton();
-deleteButton();
 
-// Take input and submit it as a new todo
-inputButton.addEventListener('click', function () {
-    var inputValue = input.value;
-    placeForTodos.innerHTML +=
-        '<div class="todo">' +
-        '<div class="well">' +
-        '<h3 class="todo-text">' + inputValue + '.' + '</h3>' +
-        '<input type="button" class="btn btn-outline-light delBtn" value="Delete">' +
-        '<input type="button" class="btn btn-outline-light completeBtn" value="Complete">' +
-        '</div>' +
-        '</div>';
-    completeButton();
-    deleteButton();
+inputButton.addEventListener('click', function() {
+    addTodo();
+    fetchTodoLast();
+    input.value = '';
 });
 
-// If enter is pressed, call the add todo function
 input.addEventListener('keyup', function (e) {
     if (e.which === 13 || e.keyCode === 13) {
-        var inputValue = input.value;
-        placeForTodos.innerHTML +=
-            '<div class="todo">' +
-            '<div class="well">' +
-            '<h3 class="todo-text">' + inputValue + '.' + '</h3>' +
-            '<input type="button" class="btn btn-outline-light delBtn" value="Delete">' +
-            '<input type="button" class="btn btn-outline-light completeBtn" value="Complete">' +
-            '</div>' +
-            '</div>';
-        completeButton();
-        deleteButton();
+        addTodo();
+        fetchTodoLast();
+        input.value = '';
     }
 });
 
@@ -49,11 +42,58 @@ function completeButton() {
     }
 }
 
-function deleteButton() {
-    var deleteButtons = document.getElementsByClassName('delBtn');
-    for (var i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener('click', function () {
-            this.parentElement.style.display = 'none';
-        });
+function removeButton() {
+    var buttons = document.getElementsByClassName('remBtn');
+    var todos = JSON.parse(localStorage.getItem('task'));
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            var selector = this.parentElement.children[0].innerHTML;
+            for (var i = 0; i < todos.length; i++) {
+                if (selector == todos[i].name) {
+                    todos.splice(i, 1);
+                    this.parentElement.style.display = 'none';
+                }
+            }
+            localStorage.setItem('task', JSON.stringify(todos));
+        })
     }
 }
+
+// LOCAL STORAGE
+
+// On submit, take the value, set it into a variable and then set the value in localStorage.
+
+var myStorage = window.localStorage;
+
+function addTodo() {
+    var todo = {
+        name: input.value
+    }
+    if (myStorage.getItem('task') === null) {
+        var myTodo = [];
+        myTodo.push(todo);
+        myStorage.setItem('task', JSON.stringify(myTodo));
+    } else {
+        var myTask = localStorage.getItem('task');
+        var storageArray = JSON.parse(myTask);
+        storageArray.push(todo);
+        localStorage.setItem('task', JSON.stringify(storageArray))
+    }
+}
+
+function fetchTodos() {
+    console.log('Fetching')
+    var todos = JSON.parse(myStorage.getItem('task'));
+    for (var i = 0; i < todos.length; i++) {
+        placeForTodos.innerHTML +=
+        markup + todos[i].name + markupend;
+    }
+}
+
+function fetchTodoLast() {
+    var todos = JSON.parse(myStorage.getItem('task'));
+        placeForTodos.innerHTML +=
+        markup + todos[todos.length - 1].name + markupend;
+        completeButton();
+        removeButton();
+    }
